73e71d4bb14962381e22f37351ca3a8c3924bd82
Nichole Henry <nichole.henry12@gmail.com>
2019-10-21 13:16:41 -0500

    test passed

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,8 +6,33 @@ namespace Palindrome
     {
         public static void Main()
         {
+            Console.WriteLine("Please enter a word");
+
+            string wordChoice = Console.ReadLine();
+
+            bool result = IsPalindrome(wordChoice);
+
+            Console.WriteLine($"{wordChoice}= Palindrome: {result}");
             Console.ReadLine();
         }
+
+        public static bool IsPalindrome(string wordChoice)
+        {
+            wordChoice = wordChoice.ToLower();
+            char[] splitWordChoice = wordChoice.ToCharArray();
+            Array.Reverse(splitWordChoice);
+            string wordReverse = string.Concat(splitWordChoice);
+
+            if (wordReverse == wordChoice)
+            {
+                return true;
+            }
+
+            else
+                return false;
+
+        }
+
 
         // TODO: Create a method that recognizes palindromes.
         // The name of the method you create should be IsPalindrome
